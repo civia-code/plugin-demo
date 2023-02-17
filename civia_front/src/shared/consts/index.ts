95552664dfd908d0fa5d564355b303aa01f42ccb
civia-code <124884930+civia-code@users.noreply.github.com>
2023-02-17 16:55:48 +0800

    Add files via upload
    civia update

---
+++ civia_front/src/shared/consts/index.ts
@@ -0,0 +1,11 @@
+const sbtConstractAddress = '0x064d96d986322534b0da0533dc0bda0c65a49274d2cd4f8e89bace5726f2be0d';
+const idConstractAddress = '0x03a6d2e88d410e040e5b1409e6452025e9c06bdd1abb4f5217b823c5fe15fd1a';
+const domainConstractAddress = '0x02486de8d181129f77382a4eebd63c8a34318d460ee472848aaba740a5039353';
+const priceConstractAddress = '0x035ac2c63c1971da29089874955a1c4cdafe698b491f8746bc70ff83788f6096';
+
+export {
+    sbtConstractAddress,
+    idConstractAddress,
+    domainConstractAddress,
+    priceConstractAddress
+};
