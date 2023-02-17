85643b7d18ac0f70d4c71df09f32a652013fa7f3
civia-code <124884930+civia-code@users.noreply.github.com>
2023-02-17 16:37:22 +0800

    Add files via upload
    civia update

---
+++ civia_front/src/inpage/index.ts
@@ -0,0 +1,108 @@
+// import { assertNever } from './../ui/services/assertNever';
+// import type { WindowMessageType } from '../shared/messages';
+// import { getProvider } from '../shared/network/provider';
+// import { disconnectAccount } from './account';
+// import { ArgentXAccount } from './ArgentXAccount';
+// import { getIsPreauthorized } from './preAuthorization';
+// import { starknetWindowObject, userEventHandlers } from './starknetWindowObject';
+
+// function attach () {
+//     try {
+//         delete window.starknet;
+//         // set read only property to window
+//         Object.defineProperty(window, 'starknet', {
+//             value: starknetWindowObject,
+//             writable: false
+//         });
+//     } catch {
+//     // ignore
+//     }
+//     // we need 2 different try catch blocks because we want to execute both even if one of them fails
+//     try {
+//         window.starknet = starknetWindowObject;
+//     } catch {
+//     // ignore
+//     }
+//     try {
+//         delete (window as any)['starknet-civia'];
+//         // set read only property to window
+//         Object.defineProperty(window, 'starknet-civia', {
+//             value: starknetWindowObject,
+//             writable: false
+//         });
+//     } catch {
+//     // ignore
+//     }
+//     try {
+//         (window as any)['starknet-civia'] = starknetWindowObject;
+//     } catch {
+//     // ignore
+//     }
+// }
+
+// function attachHandler () {
+//     attach();
+//     setTimeout(attach, 100);
+// }
+// // inject script
+// attachHandler();
+// window.addEventListener('load', () => attachHandler());
+// document.addEventListener('DOMContentLoaded', () => attachHandler());
+// document.addEventListener('readystatechange', () => attachHandler());
+
+// window.addEventListener(
+//     'message',
+//     async ({ data }: MessageEvent<WindowMessageType>) => {
+//         const { starknet } = window;
+//         if (!starknet) {
+//             return;
+//         }
+
+//         if (starknet.account && data.type === 'CONNECT_ACCOUNT') {
+//             const { address, network } = data.data;
+//             const isPreauthorized = await getIsPreauthorized();
+//             if (!isPreauthorized) {
+//                 // disconnect so the user can see they are no longer connected
+//                 // TODO: better UX would be to also re-connect when user selects pre-authorized account
+//                 await disconnectAccount();
+//             } else {
+//                 if (
+//                     address !== starknet.selectedAddress ||
+//           network.chainId !== starknet.chainId
+//                 ) {
+//                     starknet.selectedAddress = address;
+//                     starknet.chainId = network.chainId;
+//                     starknet.provider = getProvider(network);
+//                     starknet.account = new ArgentXAccount(address, starknet.provider);
+//                     for (const userEvent of userEventHandlers) {
+//                         if (userEvent.type === 'accountsChanged') {
+//                             userEvent.handler([address]);
+//                         } else if (userEvent.type === 'networkChanged') {
+//                             userEvent.handler(network.chainId);
+//                         } else {
+//                             assertNever(userEvent);
+//                         }
+//                     }
+//                 }
+//             }
+//         } else if (data.type === 'DISCONNECT_ACCOUNT') {
+//             starknet.selectedAddress = undefined;
+//             starknet.account = undefined;
+//             starknet.isConnected = false;
+//             for (const userEvent of userEventHandlers) {
+//                 if (userEvent.type === 'accountsChanged') {
+//                     userEvent.handler([]);
+//                 } else if (userEvent.type === 'networkChanged') {
+//                     userEvent.handler(undefined);
+//                 } else {
+//                     assertNever(userEvent);
+//                 }
+//             }
+//         }
+//     }
+// );
+
+import * as inPage from '@argentx/packages/extension/src/inpage/index';
+
+console.log('---inpage');
+console.log(inPage);
