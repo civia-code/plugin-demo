d9629cfa30f3a4a9e5a6712504fdd4cbf3013e27
civia-code <124884930+civia-code@users.noreply.github.com>
2023-02-17 16:58:47 +0800

    Add files via upload
    civia update

--- civia_front/src/shared/messages/AccountSocalMessage.ts
+++ civia_front/src/shared/messages/AccountSocalMessage.ts
@@ -7,14 +7,6 @@ export type AccountSocalMessage =
   | { type: 'CIVIA_CREATE_SBT_RES'; data: any }
   | { type: 'CIVIA_CREATE_SBT_RJS'; data: any }
 
-  | { type: 'CIVIA_SOCAL_FOLLOW'; data?: any }
-  | { type: 'CIVIA_SOCAL_FOLLOW_RES'; data: any }
-  | { type: 'CIVIA_SOCAL_FOLLOW_RJS'; data: any }
-
-  | { type: 'CIVIA_SOCAL_UNFOLLOW'; data?: any }
-  | { type: 'CIVIA_SOCAL_UNFOLLOW_RES'; data: any }
-  | { type: 'CIVIA_SOCAL_UNFOLLOW_RJS'; data: any }
-
   | { type: 'CIVIA_GET_ALL_FOLLOWS'; data?: any }
   | { type: 'CIVIA_GET_ALL_FOLLOWS_RES'; data: any }
   | { type: 'CIVIA_GET_ALL_FOLLOWS_RJS'; data: any }
